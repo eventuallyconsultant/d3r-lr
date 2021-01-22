0cdf9f5a9b3d288f3994f1d9591d850507d93be7
Corentin Gouil <corentin.gouil@etudiant.univ-lr.fr>
2021-01-22 21:06:18 +0100

    Création d'une base pour l'API

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use tide::Request;
+use tide::prelude::*;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    
+    app.at("/boite-aux-lettres").get(get_boite_aux_lettres);
+    app.at("/boite-aux-lettres").post(post_boite_aux_lettres);
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn get_boite_aux_lettres(mut req: Request<()>) -> tide::Result {
+    Ok(format!("You won't get anything here !").into())
+}
+
+async fn post_boite_aux_lettres(mut req: Request<()>) -> tide::Result {
+    let body: String = req.body_string().await?;
+
+    dbg!(&body);
+
+    if(body.eq("")){
+        Ok(format!("Didn't you forget something ?").into())
+    }
+    else{
+        Ok(format!("Sending invitation").into())
+    }
+}
